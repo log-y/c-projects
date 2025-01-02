f95f1ce8bbd77201041d1e622b8a46201627b987
logany <logan.yuan@gmail.com>
2025-01-02 12:51:33 -0500

    uarray

---
+++ new-array-lab/uarray2.h
@@ -0,0 +1,20 @@
+#ifndef UARRAY_INCLUDED
+#define UARRAY_INCLUDED
+
+#define T UArray_T
+typedef struct T *T;
+
+extern T     UArray_new   (int length, int size);
+extern void  UArray_free  (T *uarray);
+
+extern int   UArray_length(T  uarray);
+extern int   UArray_size  (T  uarray);
+
+extern void *UArray_at    (T  uarray, int i);
+
+extern void UArray_resize (T  uarray, int length);
+extern T    UArray_copy   (T  uarray, int length);
+
+#undef T
+
+#endif
